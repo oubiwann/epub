cccf9c1cbf6873295c7711d12556957b937e7b77
Florian Strzelecki <florian.strzelecki@gmail.com>
2012-02-06 17:26:03 +0100

    function as_xml_element for EpubManifestItem and EpubMetadata

--- epub/__init__.py
+++ epub/__init__.py
@@ -350,6 +350,108 @@ class EpubMetadata(object):
             isbn = l[0]
         return isbn
 
+    def as_xml_element(self):
+        """Return an xml dom Element node."""
+        doc = minidom.Document()
+        metadata = doc.createElement('metadata')
+
+        for text, lang in self.title:
+            title = doc.createElement('dc:title')
+            if lang:
+                title.setAttribute('xml:lang', lang)
+            title.appendChild(doc.createTextNode(text))
+            metadata.appendChild(title)
+
+        for name, role, file_as in self.creator:
+            creator = doc.createElement('dc:creator')
+            creator.setAttribute('opf:role', role)
+            if file_as:
+                creator.setAttribute('opf:file-as', file_as)
+            creator.appendChild(doc.createTextNode(name))
+            metadata.appendChild(creator)
+
+        for text in self.subject:
+            subject = doc.createElement('dc:subject')
+            subject.appendChild(doc.createTextNode(text))
+            metadata.appendChild(subject)
+
+        if self.description:
+            description = doc.createElement('dc:description')
+            description.appendChild(doc.createTextNode(self.description))
+            metadata.appendChild(description)
+
+        if self.publisher:
+            publisher = doc.createElement('dc:publisher')
+            publisher.appendChild(doc.createTextNode(self.publisher))
+            metadata.appendChild(publisher)
+
+        for name, role, file_as in self.contributor:
+            contributor = doc.createElement('dc:creator')
+            contributor.setAttribute('opf:role', role)
+            if file_as:
+                contributor.setAttribute('opf:file-as', file_as)
+            contributor.appendChild(doc.createTextNode(name))
+            metadata.appendChild(contributor)
+
+        for text, event in self.date:
+            date = doc.createElement('dc:date')
+            if event:
+                date.setAttribute('opf:event', event)
+            date.appendChild(doc.createTextNode(text))
+            metadata.appendChild(date)
+
+        if self.type:
+            type = doc.createElement('dc:type')
+            type.appendChild(doc.createTextNode(self.type))
+            metadata.appendChild(type)
+
+        if self.format:
+            format = doc.createElement('dc:format')
+            format.appendChild(doc.createTextNode(self.format))
+            metadata.appendChild(format)
+
+        for text, id, scheme in self.identifier:
+            identifier = doc.createElement('dc:identifier')
+            if id:
+                identifier.setAttribute('id', id)
+            if scheme:
+                identifier.setAttribute('opf:scheme', scheme)
+            identifier.appendChild(doc.createTextNode(text))
+            metadata.appendChild(identifier)
+
+        if self.source:
+            source = doc.createElement('dc:source')
+            source.appendChild(doc.createTextNode(self.source))
+            metadata.appendChild(source)
+
+        for text in self.language:
+            language = doc.createElement('dc:language')
+            language.appendChild(doc.createTextNode(text))
+            metadata.appendChild(language)
+
+        if self.relation:
+            relation = doc.createElement('dc:relation')
+            relation.appendChild(doc.createTextNode(self.relation))
+            metadata.appendChild(relation)
+
+        if self.coverage:
+            coverage = doc.createElement('dc:coverage')
+            coverage.appendChild(doc.createTextNode(self.coverage))
+            metadata.appendChild(coverage)
+
+        if self.rights:
+            rights = doc.createElement('dc:rights')
+            rights.appendChild(doc.createTextNode(self.rights))
+            metadata.appendChild(rights)
+
+        for name, content in self.meta:
+            meta = doc.createElement('meta')
+            meta.setAttribute('name', name)
+            meta.setAttribute('content', content)
+            metadata.appendChild(meta)
+
+        return metadata
+
 
 class EpubManifestItem(object):
     """Represent an item from the epub's manifest."""
@@ -372,3 +474,24 @@ class EpubManifestItem(object):
         self.required_namespace = required_namespace
         self.required_modules = required_modules
         self.fallback_style = fallback_style
+
+    def as_xml_element(self):
+        """Return an xml dom Element node."""
+
+        item = minidom.Document().createElement("item")
+
+        item.setAttribute('id', self.id)
+        item.setAttribute('href', self.href)
+        if self.media_type:
+            item.setAttribute('media_type', self.media_type)
+        if self.fallback:
+            item.setAttribute('fallback', self.fallback)
+        if self.required_namespace:
+            item.setAttribute('required_namespace', self.required_namespace)
+        if self.required_modules:
+            item.setAttribute('required_modules', self.required_modules)
+        if self.fallback_style:
+            item.setAttribute('fallback_style', self.fallback_style)
+
+        return item
+
