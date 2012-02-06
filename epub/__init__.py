28acf2c9521e3911b6ff38f703889b53847d9a62
Florian Strzelecki <florian.strzelecki@gmail.com>
2012-02-06 17:56:40 +0100

    Set version to 0.1.1 (this is only a beta version)

--- epub/__init__.py
+++ epub/__init__.py
@@ -5,7 +5,7 @@ Library to open and read files in the epub version 2.
 """
 
 __author__ = u'Florian Strzelecki <florian.strzelecki@gmail.com>'
-__version__ = u'0.1.0'
+__version__ = u'0.1.1'
 
 import os
 import zipfile
