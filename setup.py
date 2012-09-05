add2c1899a77cf3de8ec558713473d22e2c605aa
Florian Strzelecki <florian.strzelecki@gmail.com>
2012-09-05 20:53:44 +0200

    Remove unicode string and fixes #1

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from distutils.core import setup
 
 
@@ -7,17 +6,17 @@ with open("README.txt") as f:
     long_description = f.read()
 
 
-setup(name=u'epub',
-      version=u'0.5.0',
-      description=u'Library to open and read files in the epub version 2.',
+setup(name='epub',
+      version='0.5.0',
+      description='Library to open and read files in the epub version 2.',
       long_description=long_description,
-      author=u'Florian Strzelecki',
-      author_email=u'florian.strzelecki@gmail.com',
-      license=u'LGPL',
-      url=u'http://epub.exirel.me',
-      packages=[u'epub'],
-      classifiers=[u'Development Status :: 4 - Beta',
-                  u'Intended Audience :: Developers',
-                  u'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-                  u'Programming Language :: Python :: 2.7',
-                  u'Topic :: Software Development :: Libraries :: Python Modules'])
+      author='Florian Strzelecki',
+      author_email='florian.strzelecki@gmail.com',
+      license='LGPL',
+      url='http://epub.exirel.me',
+      packages=['epub',],
+      classifiers=['Development Status :: 4 - Beta',
+                  'Intended Audience :: Developers',
+                  'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+                  'Programming Language :: Python :: 2.7',
+                  'Topic :: Software Development :: Libraries :: Python Modules'])
