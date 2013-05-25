5824c4576bb3628eb639faad1c749aedecd07d6e
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-05-25 15:40:26 -0700

    Added new constants file.

---
+++ epub/const.py
@@ -0,0 +1,6 @@
+MIMETYPE_EPUB = 'application/epub+zip'
+MIMETYPE_OPF = 'application/oebps-package+xml'
+MIMETYPE_NCX = 'application/x-dtbncx+xml'
+
+DEFAULT_OPF_PATH = 'OEBPS/content.opf'
+DEFAULT_NCX_PATH = 'toc.ncx'
